e999c524deb4b048a5f1eaf8cacef197216ec6a4
markjun05650 <markjun05650@gmail.com>
2016-07-10 23:09:28 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,19 @@
-package com.pl.main;
+package bukkit.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+  public final  Logger logger = Logger.getLogger("Minecraft");
+  public static main plugin;
+  
+  public void onDisable() {
+    PluginDescriptionFile pdfFile = this.getDescription();
+    this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+  }
+  
+  public void onEnable() {
+    PluginDescriptionFile pdfFile = this.getDescription();
+    this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
+  }
+  
+}
